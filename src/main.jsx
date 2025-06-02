556baa73ea446e4390c0a6c13a8c7a0a7fdb5316
s-a-m-r-a <aliyevasemra04@gmail.com>
2025-06-02 21:42:02 +0400

    error fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from "./context/AuthContext";
 import './App.css';
 createRoot(document.getElementById('root')).render(
-<BrowserRouter basename='/midify'>
+<HashRouter>
   <AuthProvider>
       <App />
   </AuthProvider>
-</BrowserRouter>,
+</HashRouter>
 )
